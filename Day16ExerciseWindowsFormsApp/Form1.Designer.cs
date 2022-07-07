70197ffa464c17d7200b53430472220f7d6dd27c
Niraj Kayastha <Niraj Kayastha@LAPTOP-6FIM74JD>
2022-07-07 23:53:23 +0530

    Add project files.

---
+++ Day16ExerciseWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace Day16ExerciseWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtEmaild = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboGender = new System.Windows.Forms.ComboBox();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblBirthDate = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.lblEmailId = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblConfirmPassword = new System.Windows.Forms.Label();
+            this.lblRegistration = new System.Windows.Forms.Label();
+            this.btnSignIn = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(364, 69);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(148, 27);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(364, 135);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(148, 27);
+            this.txtLastName.TabIndex = 1;
+            // 
+            // txtEmaild
+            // 
+            this.txtEmaild.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmaild.Location = new System.Drawing.Point(364, 372);
+            this.txtEmaild.Name = "txtEmaild";
+            this.txtEmaild.Size = new System.Drawing.Size(148, 27);
+            this.txtEmaild.TabIndex = 2;
+            this.txtEmaild.TextChanged += new System.EventHandler(this.txtEmaild_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(364, 440);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(148, 27);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPassword.Location = new System.Drawing.Point(364, 501);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(148, 27);
+            this.txtConfirmPassword.TabIndex = 4;
+            this.txtConfirmPassword.UseSystemPasswordChar = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(364, 213);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(148, 27);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // comboGender
+            // 
+            this.comboGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboGender.FormattingEnabled = true;
+            this.comboGender.Location = new System.Drawing.Point(364, 298);
+            this.comboGender.Name = "comboGender";
+            this.comboGender.Size = new System.Drawing.Size(148, 28);
+            this.comboGender.TabIndex = 6;
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstName.Location = new System.Drawing.Point(132, 69);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(102, 20);
+            this.lblFirstName.TabIndex = 7;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastName.Location = new System.Drawing.Point(132, 142);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(100, 20);
+            this.lblLastName.TabIndex = 8;
+            this.lblLastName.Text = "Last Name";
+            // 
+            // lblBirthDate
+            // 
+            this.lblBirthDate.AutoSize = true;
+            this.lblBirthDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBirthDate.Location = new System.Drawing.Point(132, 218);
+            this.lblBirthDate.Name = "lblBirthDate";
+            this.lblBirthDate.Size = new System.Drawing.Size(96, 20);
+            this.lblBirthDate.TabIndex = 9;
+            this.lblBirthDate.Text = "Birth Date";
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Location = new System.Drawing.Point(132, 298);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(70, 20);
+            this.lblGender.TabIndex = 10;
+            this.lblGender.Text = "Gender";
+            // 
+            // lblEmailId
+            // 
+            this.lblEmailId.AutoSize = true;
+            this.lblEmailId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmailId.Location = new System.Drawing.Point(132, 379);
+            this.lblEmailId.Name = "lblEmailId";
+            this.lblEmailId.Size = new System.Drawing.Size(77, 20);
+            this.lblEmailId.TabIndex = 11;
+            this.lblEmailId.Text = "Email Id";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(132, 447);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(91, 20);
+            this.lblPassword.TabIndex = 12;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblConfirmPassword
+            // 
+            this.lblConfirmPassword.AutoSize = true;
+            this.lblConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblConfirmPassword.Location = new System.Drawing.Point(132, 507);
+            this.lblConfirmPassword.Name = "lblConfirmPassword";
+            this.lblConfirmPassword.Size = new System.Drawing.Size(163, 20);
+            this.lblConfirmPassword.TabIndex = 13;
+            this.lblConfirmPassword.Text = "Confirm Password";
+            // 
+            // lblRegistration
+            // 
+            this.lblRegistration.AutoSize = true;
+            this.lblRegistration.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegistration.ForeColor = System.Drawing.Color.Brown;
+            this.lblRegistration.Location = new System.Drawing.Point(282, 9);
+            this.lblRegistration.Name = "lblRegistration";
+            this.lblRegistration.Size = new System.Drawing.Size(179, 32);
+            this.lblRegistration.TabIndex = 14;
+            this.lblRegistration.Text = "Registration";
+            // 
+            // btnSignIn
+            // 
+            this.btnSignIn.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnSignIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignIn.Location = new System.Drawing.Point(136, 593);
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.Size = new System.Drawing.Size(114, 36);
+            this.btnSignIn.TabIndex = 15;
+            this.btnSignIn.Text = "Sign In";
+            this.btnSignIn.UseVisualStyleBackColor = false;
+            this.btnSignIn.Click += new System.EventHandler(this.btnSignIn_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.Red;
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(388, 593);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(114, 36);
+            this.btnReset.TabIndex = 16;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.ClientSize = new System.Drawing.Size(739, 697);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSignIn);
+            this.Controls.Add(this.lblRegistration);
+            this.Controls.Add(this.lblConfirmPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblEmailId);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblBirthDate);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.comboGender);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtEmaild);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtEmaild;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboGender;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblBirthDate;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.Label lblEmailId;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblConfirmPassword;
+        private System.Windows.Forms.Label lblRegistration;
+        private System.Windows.Forms.Button btnSignIn;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ Day16ExerciseWindowsFormsApp/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Day16ExerciseWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection con = null;
+        private SqlCommand cmd = null;
+        private SqlDataReader reader = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //add values in combobox
+            comboGender.Text = "--Select--";
+            comboGender.Items.Add("Male");
+            comboGender.Items.Add("Female");
+            comboGender.Items.Add("Others");
+       
+
+        }
+        private void ValidateEmail()
+        {
+            try
+            {   // email validation
+                string email = txtEmaild.Text;
+                Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                Match match = regex.Match(email);
+                if (match.Success)
+                    MessageBox.Show(email + " is Valid Email Address");
+                
+                else
+                    MessageBox.Show(email + " is Invalid Email Address");
+                  // ClearText();
+                //txtEmaild.Focus();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+           
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            ValidateEmail();
+          
+            try
+            {
+                using (con = new SqlConnection(ConfigurationManager.ConnectionStrings["HRConn"].ConnectionString))
+                {
+                    using (cmd = new SqlCommand("usp_AddNewUser", con)) // sp to add new user
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.AddWithValue("@First_Name", txtFirstName.Text);
+                        cmd.Parameters.AddWithValue("@Last_Name", txtLastName.Text);
+                        cmd.Parameters.AddWithValue("@Birthdate", dateTimePicker1.Text);
+                        cmd.Parameters.AddWithValue("@Gender", comboGender.Text);
+                        cmd.Parameters.AddWithValue("@Emaild", txtEmaild.Text);
+                        cmd.Parameters.AddWithValue("@Password", txtPassword.Text);
+                        cmd.Parameters.AddWithValue("@ConfirmPassword", txtConfirmPassword.Text);
+
+
+                        if (con.State == ConnectionState.Closed)
+                        {
+                            con.Open();
+
+                        }
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("New User Created");
+            }
+            catch (Exception ex)
+            {
+
+         
+            }
+          
+
+
+        }
+        private void ClearText()
+        {
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            comboGender.Text = "";
+            txtEmaild.Text = "";
+            txtPassword.Text = "";
+            txtConfirmPassword.Text = "";
+
+        }
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ClearText();//method call
+    
+        }
+
+        private void txtEmaild_TextChanged(object sender, EventArgs e)
+        {
+            //ValidateEmail(); //method call
+        }
+    }
+}
